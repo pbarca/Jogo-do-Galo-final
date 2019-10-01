a8694428f3b797691846ad3fc433fc97274a48f3
Paulo Coto <Paulo Coto@DESKTOP-QBHA61I>
2019-10-01 12:04:09 +0100

    primeiro push

---
+++ JogoGalo/Form1.Designer.vb
@@ -0,0 +1,207 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.Button1 = New System.Windows.Forms.Button()
+        Me.Label2 = New System.Windows.Forms.Label()
+        Me.PictureBox9 = New System.Windows.Forms.PictureBox()
+        Me.PictureBox6 = New System.Windows.Forms.PictureBox()
+        Me.PictureBox3 = New System.Windows.Forms.PictureBox()
+        Me.PictureBox8 = New System.Windows.Forms.PictureBox()
+        Me.PictureBox5 = New System.Windows.Forms.PictureBox()
+        Me.PictureBox2 = New System.Windows.Forms.PictureBox()
+        Me.PictureBox7 = New System.Windows.Forms.PictureBox()
+        Me.PictureBox4 = New System.Windows.Forms.PictureBox()
+        Me.PictureBox1 = New System.Windows.Forms.PictureBox()
+        CType(Me.PictureBox9, System.ComponentModel.ISupportInitialize).BeginInit()
+        CType(Me.PictureBox6, System.ComponentModel.ISupportInitialize).BeginInit()
+        CType(Me.PictureBox3, System.ComponentModel.ISupportInitialize).BeginInit()
+        CType(Me.PictureBox8, System.ComponentModel.ISupportInitialize).BeginInit()
+        CType(Me.PictureBox5, System.ComponentModel.ISupportInitialize).BeginInit()
+        CType(Me.PictureBox2, System.ComponentModel.ISupportInitialize).BeginInit()
+        CType(Me.PictureBox7, System.ComponentModel.ISupportInitialize).BeginInit()
+        CType(Me.PictureBox4, System.ComponentModel.ISupportInitialize).BeginInit()
+        CType(Me.PictureBox1, System.ComponentModel.ISupportInitialize).BeginInit()
+        Me.SuspendLayout()
+        '
+        'Button1
+        '
+        Me.Button1.BackColor = System.Drawing.SystemColors.ButtonShadow
+        Me.Button1.Font = New System.Drawing.Font("Microsoft Sans Serif", 12.0!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Button1.ForeColor = System.Drawing.Color.OrangeRed
+        Me.Button1.Location = New System.Drawing.Point(277, 624)
+        Me.Button1.Margin = New System.Windows.Forms.Padding(4)
+        Me.Button1.Name = "Button1"
+        Me.Button1.Size = New System.Drawing.Size(235, 47)
+        Me.Button1.TabIndex = 0
+        Me.Button1.Text = "Novo Jogo"
+        Me.Button1.UseVisualStyleBackColor = False
+        '
+        'Label2
+        '
+        Me.Label2.AutoSize = True
+        Me.Label2.Font = New System.Drawing.Font("Monotype Corsiva", 30.0!, CType((System.Drawing.FontStyle.Bold Or System.Drawing.FontStyle.Italic), System.Drawing.FontStyle), System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label2.ForeColor = System.Drawing.SystemColors.Desktop
+        Me.Label2.Location = New System.Drawing.Point(249, 11)
+        Me.Label2.Margin = New System.Windows.Forms.Padding(4, 0, 4, 0)
+        Me.Label2.Name = "Label2"
+        Me.Label2.Size = New System.Drawing.Size(265, 60)
+        Me.Label2.TabIndex = 2
+        Me.Label2.Text = "Jogo do Galo"
+        '
+        'PictureBox9
+        '
+        Me.PictureBox9.Image = Global.JogoGalo.My.Resources.Resources.Vazio
+        Me.PictureBox9.Location = New System.Drawing.Point(528, 448)
+        Me.PictureBox9.Margin = New System.Windows.Forms.Padding(4)
+        Me.PictureBox9.Name = "PictureBox9"
+        Me.PictureBox9.Size = New System.Drawing.Size(171, 158)
+        Me.PictureBox9.TabIndex = 4
+        Me.PictureBox9.TabStop = False
+        '
+        'PictureBox6
+        '
+        Me.PictureBox6.Image = Global.JogoGalo.My.Resources.Resources.Vazio
+        Me.PictureBox6.Location = New System.Drawing.Point(528, 272)
+        Me.PictureBox6.Margin = New System.Windows.Forms.Padding(4)
+        Me.PictureBox6.Name = "PictureBox6"
+        Me.PictureBox6.Size = New System.Drawing.Size(171, 158)
+        Me.PictureBox6.TabIndex = 4
+        Me.PictureBox6.TabStop = False
+        '
+        'PictureBox3
+        '
+        Me.PictureBox3.Image = Global.JogoGalo.My.Resources.Resources.Vazio
+        Me.PictureBox3.Location = New System.Drawing.Point(528, 92)
+        Me.PictureBox3.Margin = New System.Windows.Forms.Padding(4)
+        Me.PictureBox3.Name = "PictureBox3"
+        Me.PictureBox3.Size = New System.Drawing.Size(171, 158)
+        Me.PictureBox3.TabIndex = 4
+        Me.PictureBox3.TabStop = False
+        '
+        'PictureBox8
+        '
+        Me.PictureBox8.Image = Global.JogoGalo.My.Resources.Resources.Vazio
+        Me.PictureBox8.Location = New System.Drawing.Point(308, 448)
+        Me.PictureBox8.Margin = New System.Windows.Forms.Padding(4)
+        Me.PictureBox8.Name = "PictureBox8"
+        Me.PictureBox8.Size = New System.Drawing.Size(171, 158)
+        Me.PictureBox8.TabIndex = 4
+        Me.PictureBox8.TabStop = False
+        '
+        'PictureBox5
+        '
+        Me.PictureBox5.Image = Global.JogoGalo.My.Resources.Resources.Vazio
+        Me.PictureBox5.Location = New System.Drawing.Point(308, 272)
+        Me.PictureBox5.Margin = New System.Windows.Forms.Padding(4)
+        Me.PictureBox5.Name = "PictureBox5"
+        Me.PictureBox5.Size = New System.Drawing.Size(171, 158)
+        Me.PictureBox5.TabIndex = 4
+        Me.PictureBox5.TabStop = False
+        '
+        'PictureBox2
+        '
+        Me.PictureBox2.Image = Global.JogoGalo.My.Resources.Resources.Vazio
+        Me.PictureBox2.Location = New System.Drawing.Point(308, 92)
+        Me.PictureBox2.Margin = New System.Windows.Forms.Padding(4)
+        Me.PictureBox2.Name = "PictureBox2"
+        Me.PictureBox2.Size = New System.Drawing.Size(171, 158)
+        Me.PictureBox2.TabIndex = 4
+        Me.PictureBox2.TabStop = False
+        '
+        'PictureBox7
+        '
+        Me.PictureBox7.Image = Global.JogoGalo.My.Resources.Resources.Vazio
+        Me.PictureBox7.Location = New System.Drawing.Point(91, 448)
+        Me.PictureBox7.Margin = New System.Windows.Forms.Padding(4)
+        Me.PictureBox7.Name = "PictureBox7"
+        Me.PictureBox7.Size = New System.Drawing.Size(171, 158)
+        Me.PictureBox7.TabIndex = 4
+        Me.PictureBox7.TabStop = False
+        '
+        'PictureBox4
+        '
+        Me.PictureBox4.Image = Global.JogoGalo.My.Resources.Resources.Vazio
+        Me.PictureBox4.Location = New System.Drawing.Point(91, 272)
+        Me.PictureBox4.Margin = New System.Windows.Forms.Padding(4)
+        Me.PictureBox4.Name = "PictureBox4"
+        Me.PictureBox4.Size = New System.Drawing.Size(171, 158)
+        Me.PictureBox4.TabIndex = 4
+        Me.PictureBox4.TabStop = False
+        '
+        'PictureBox1
+        '
+        Me.PictureBox1.Image = Global.JogoGalo.My.Resources.Resources.Vazio
+        Me.PictureBox1.Location = New System.Drawing.Point(91, 92)
+        Me.PictureBox1.Margin = New System.Windows.Forms.Padding(4)
+        Me.PictureBox1.Name = "PictureBox1"
+        Me.PictureBox1.Size = New System.Drawing.Size(171, 158)
+        Me.PictureBox1.TabIndex = 4
+        Me.PictureBox1.TabStop = False
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(8.0!, 16.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.BackColor = System.Drawing.SystemColors.ActiveBorder
+        Me.ClientSize = New System.Drawing.Size(789, 697)
+        Me.Controls.Add(Me.PictureBox9)
+        Me.Controls.Add(Me.PictureBox6)
+        Me.Controls.Add(Me.PictureBox3)
+        Me.Controls.Add(Me.PictureBox8)
+        Me.Controls.Add(Me.PictureBox5)
+        Me.Controls.Add(Me.PictureBox2)
+        Me.Controls.Add(Me.PictureBox7)
+        Me.Controls.Add(Me.PictureBox4)
+        Me.Controls.Add(Me.PictureBox1)
+        Me.Controls.Add(Me.Label2)
+        Me.Controls.Add(Me.Button1)
+        Me.Margin = New System.Windows.Forms.Padding(4)
+        Me.Name = "Form1"
+        Me.Text = "Form1"
+        CType(Me.PictureBox9, System.ComponentModel.ISupportInitialize).EndInit()
+        CType(Me.PictureBox6, System.ComponentModel.ISupportInitialize).EndInit()
+        CType(Me.PictureBox3, System.ComponentModel.ISupportInitialize).EndInit()
+        CType(Me.PictureBox8, System.ComponentModel.ISupportInitialize).EndInit()
+        CType(Me.PictureBox5, System.ComponentModel.ISupportInitialize).EndInit()
+        CType(Me.PictureBox2, System.ComponentModel.ISupportInitialize).EndInit()
+        CType(Me.PictureBox7, System.ComponentModel.ISupportInitialize).EndInit()
+        CType(Me.PictureBox4, System.ComponentModel.ISupportInitialize).EndInit()
+        CType(Me.PictureBox1, System.ComponentModel.ISupportInitialize).EndInit()
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+    Friend WithEvents Button1 As System.Windows.Forms.Button
+    Friend WithEvents Label2 As System.Windows.Forms.Label
+    Friend WithEvents PictureBox1 As System.Windows.Forms.PictureBox
+    Friend WithEvents PictureBox2 As System.Windows.Forms.PictureBox
+    Friend WithEvents PictureBox3 As System.Windows.Forms.PictureBox
+    Friend WithEvents PictureBox4 As System.Windows.Forms.PictureBox
+    Friend WithEvents PictureBox5 As System.Windows.Forms.PictureBox
+    Friend WithEvents PictureBox6 As System.Windows.Forms.PictureBox
+    Friend WithEvents PictureBox7 As System.Windows.Forms.PictureBox
+    Friend WithEvents PictureBox8 As System.Windows.Forms.PictureBox
+    Friend WithEvents PictureBox9 As System.Windows.Forms.PictureBox
+
+End Class
---
+++ JogoGalo/Form1.vb
@@ -0,0 +1,155 @@
+﻿
+Public Class Form1
+    Dim Ajogar, Galo(9) As Integer
+    Dim Quadros() As PictureBox
+
+    Private Sub Button1_Click(sender As System.Object, e As System.EventArgs) Handles Button1.Click
+        Call InicializaGalo()
+    End Sub
+    Private Sub InicializaGalo()
+
+        Dim i As Integer
+        For i = 0 To 8
+            Galo(i) = 0
+            Call Imagem(i, 0)
+        Next
+    End Sub
+    Private Sub PictureBox1_Click(sender As System.Object, e As System.EventArgs) Handles PictureBox1.Click, PictureBox2.Click, PictureBox3.Click, PictureBox4.Click, PictureBox5.Click, PictureBox6.Click, PictureBox7.Click, PictureBox8.Click, PictureBox9.Click
+        Dim quadro As Integer
+        Select Case sender.name
+            Case "PictureBox1"
+                quadro = 0
+            Case "PictureBox2"
+                quadro = 1
+            Case "PictureBox3"
+                quadro = 2
+            Case "PictureBox4"
+                quadro = 3
+            Case "PictureBox5"
+                quadro = 4
+            Case "PictureBox6"
+                quadro = 5
+            Case "PictureBox7"
+                quadro = 6
+            Case "PictureBox8"
+                quadro = 7
+            Case "PictureBox9"
+                quadro = 8
+        End Select
+
+
+        If (Galo(quadro) = 0) Then
+
+            Call Imagem(quadro, 1)
+            Call Verseganhou()
+            Call JogaPC()
+            Call Verseganhou()
+
+        End If
+
+    End Sub
+    Private Sub Imagem(quadro, img)
+        Dim fig As New PictureBox
+        If img = 1 Then
+            fig.Image = My.Resources.Cruz
+        ElseIf img = 2 Then
+            fig.Image = My.Resources.Bola
+        Else
+            fig.Image = My.Resources.Vazio
+        End If
+
+        Quadros(quadro).Image = fig.Image
+        Galo(quadro) = img
+
+
+    End Sub
+
+    Private Function M(a, b, c) As Integer
+        Dim r As Integer
+        r = Galo(a) * Galo(b) * Galo(c)
+        If r = 1 Or r = 8 Then
+            Return r
+        Else
+            Return (0)
+        End If
+
+
+    End Function
+
+    Private Sub Verseganhou()
+        Dim g As Integer
+        g = M(0, 1, 2) + M(3, 4, 5) + M(6, 7, 8) + M(0, 3, 6) + M(1, 4, 7) + M(2, 5, 8) + M(0, 4, 8) + M(2, 4, 6)
+
+        If g > 7 Then
+            MsgBox("Perdestes!!! Jogar Novamente? ")
+            Call InicializaGalo()
+        ElseIf g > 0 Then
+            MsgBox("Ganhastes!!! Jogar Novamente? ")
+            Call InicializaGalo()
+        Else
+            Dim i As Integer
+            Dim empate = True
+            For i = 0 To 8
+                If Galo(i) = 0 Then empate = False
+            Next
+            If empate Then
+                MsgBox("empatastes!!! Jogar Novamente? ")
+                Call InicializaGalo()
+            End If
+        End If
+    End Sub
+    Private Function A(q1, q2, q3, v) As Integer
+        If Galo(q1) = 0 And Galo(q2) = 2 And Galo(q3) = 2 Then
+            Return q1
+        ElseIf Galo(q1) = 2 And Galo(q2) = 0 And Galo(q3) = 2 Then
+            Return q2
+        ElseIf Galo(q1) = 2 And Galo(q2) = 2 And Galo(q3) = 0 Then
+            Return q3
+        Else : Return v
+        End If
+
+    End Function
+    Private Function D(q1, q2, q3, v) As Integer
+        If Galo(q1) = 0 And Galo(q2) = 1 And Galo(q3) = 1 Then
+            Return q1
+        ElseIf Galo(q1) = 1 And Galo(q2) = 0 And Galo(q3) = 1 Then
+            Return q2
+        ElseIf Galo(q1) = 1 And Galo(q2) = 1 And Galo(q3) = 0 Then
+            Return q3
+        Else : Return v
+        End If
+
+    End Function
+
+    Private Sub Form1_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+        Quadros = {PictureBox1, PictureBox2, PictureBox3, PictureBox4, PictureBox5, PictureBox6, PictureBox7, PictureBox8, PictureBox9}
+    End Sub
+
+    Private Sub JogaPC()
+
+        Dim i As Integer
+        Do
+            i = Int(Rnd() * 9)
+        Loop While Galo(i) > 0
+
+        i = D(0, 1, 2, i)
+        i = D(3, 4, 5, i)
+        i = D(6, 7, 8, i)
+        i = D(0, 3, 6, i)
+        i = D(1, 4, 7, i)
+        i = D(2, 5, 8, i)
+        i = D(0, 4, 8, i)
+        i = D(2, 4, 6, i)
+
+        i = A(0, 1, 2, i)
+        i = A(3, 4, 5, i)
+        i = A(6, 7, 8, i)
+        i = A(0, 3, 6, i)
+        i = A(1, 4, 7, i)
+        i = A(2, 5, 8, i)
+        i = A(0, 4, 8, i)
+        i = A(2, 4, 6, i)
+
+        Call Imagem(i, 2)
+    End Sub
+End Class
